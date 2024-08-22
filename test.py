c0942a10e5289b76347092e45a5d5de5a603ee8a
W00SHH <jerry7780@g.skku.edu>
2024-08-22 17:41:24 +0900

    테스트

--- test.py
+++ test.py
@@ -1,72 +1,31 @@
-# from airflow import DAG
-# from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-# from airflow.utils.dates import days_ago
-
-# default_args = {
-#     'owner': 'airflow',
-#     'depends_on_past': False,
-#     'start_date': days_ago(1),
-# }
-
-# dag = DAG(
-#     'pyspark_test',
-#     default_args=default_args,
-#     schedule_interval=None,
-#     catchup=False,
-# )
-
-# submit_spark_job = SparkKubernetesOperator(
-#     task_id='spark_pi_submit',
-#     namespace='airflow',  # Your Kubernetes namespace
-#     application_file='spark-pi.yaml',  # Spark application YAML file
-#     kubernetes_conn_id='k8s_default',  # Kubernetes connection ID in Airflow
-#     do_xcom_push=True,
-#     dag=dag,
-# )
-
-# submit_spark_job
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import days_ago
-from airflow.operators.dummy import DummyOperator
 
-# 기본 설정
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(1),
-    'retries': 1,
 }
 
-# DAG 정의
-dag = DAG(
-    'pyspark_test_dag',
+with DAG(
+    dag_id='pyspark_on_k8s',
     default_args=default_args,
-    description='test for DAG using pyspark',
-    schedule_interval=None,  # 스케줄링을 비활성화하고 수동으로 실행
+    schedule_interval=None,
     catchup=False,
-)
+) as dag:
 
-# 시작 태스크
-start = DummyOperator(
-    task_id='start',
-    dag=dag,
-)
+    run_spark_pi = KubernetesPodOperator(
+        task_id='run_spark_pi',
+        name='spark-pi-job',
+        namespace='airflow',
+        image='bitnami/spark:latest',
+        cmds=['spark-submit', '--master', 'k8s://https://kubernetes.default.svc',
+              '--deploy-mode', 'cluster',
+              '--class', 'org.apache.spark.examples.SparkPi',
+              'local:///opt/spark/examples/jars/spark-examples_2.12-3.0.0.jar'],
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        get_logs=True,
+    )
 
-# PySpark 작업을 실행할 SparkKubernetesOperator
-spark_task = SparkKubernetesOperator(
-    task_id='spark_pi_task',
-    namespace='airflow',  # Kubernetes 네임스페이스를 지정
-    application_file='spark-pi.yaml',  # PySpark 작업을 정의하는 YAML 파일 경로
-    kubernetes_conn_id='k8s_default',  # Airflow의 Kubernetes 연결 ID
-    do_xcom_push=True,  # 작업 결과를 XCom에 푸시
-    dag=dag,
-)
-
-# 종료 태스크
-end = DummyOperator(
-    task_id='end',
-    dag=dag,
-)
-
-# 태스크 순서 정의
-start >> spark_task >> end
+    run_spark_pi
